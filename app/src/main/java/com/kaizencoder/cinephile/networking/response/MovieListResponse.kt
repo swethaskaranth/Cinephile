b73bc0063a1a1e0a099ef4b7e2ea951f4ea237ad
Swetha S <shwetha.s.kar@gmail.com>
2025-07-25 15:20:40 +0530

    Added rating component to movie detail screen

--- app/src/main/java/com/kaizencoder/cinephile/networking/response/MovieListResponse.kt
+++ app/src/main/java/com/kaizencoder/cinephile/networking/response/MovieListResponse.kt
@@ -9,7 +9,7 @@ data class MovieListResponse(
 
 data class Movie(
     val adult: Boolean,
-    val backdrop_path: String,
+    val backdrop_path: String?,
     val genre_ids: List<Int>,
     val id: Int,
     val original_language: String,
--- app/src/main/java/com/kaizencoder/cinephile/ui/screens/MovieDetailScreen.kt
+++ app/src/main/java/com/kaizencoder/cinephile/ui/screens/MovieDetailScreen.kt
@@ -1,5 +1,6 @@
 package com.kaizencoder.cinephile.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -9,11 +10,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,8 +31,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
 import com.kaizencoder.cinephile.ui.MovieDetailViewModel
@@ -47,7 +58,14 @@ fun MovieDetailScreen(
         ) {
             val posterPath = movie.poster_path ?: ""
             item {
-                Row {
+                Text(
+                    text = movie.title,
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(top = 10.dp)
+                )
+            }
+            item {
+                Row(modifier = Modifier.padding(top = 16.dp)) {
                     AsyncImage(
                         model = "https://image.tmdb.org/t/p/w500$posterPath",
                         contentDescription = "Movie Poster",
@@ -111,6 +129,48 @@ fun MovieDetailScreen(
                                     })
                             )
                     }
+                }
+
+                Row(
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp),
+                    horizontalArrangement = Arrangement.spacedBy(10.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    Icon(
+                        imageVector = Icons.Default.Star,
+                        contentDescription = "rating",
+                        tint = Color.Yellow,
+                        modifier = Modifier.size(20.dp)
+                    )
+
+                    Text(
+                        text = buildAnnotatedString {
+                            withStyle(SpanStyle(fontWeight = FontWeight.Bold, fontSize = 16.sp)) {
+                                append(String.format("%.1f", movie.vote_average))
+                            }
+                            withStyle(
+                                SpanStyle(
+                                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f),
+                                    fontSize = 12.sp
+                                )
+                            ) {
+                                append("/10")
+                            }
+
+                        },
+                        color = Color.White,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.alignByBaseline()
+                    )
+
+                    Text(
+                        text = if (movie.vote_count > 1000) "${movie.vote_count / 1000}K" else movie.vote_count.toString(),
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f),
+                        fontSize = 12.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.alignByBaseline()
+                    )
                 }
             }
 
